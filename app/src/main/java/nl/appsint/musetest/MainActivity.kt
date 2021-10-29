585db0f3f68686b67fa038cfdad7fa806f399be0
Thijs Kok <thijs@koksoftware.com>
2021-10-29 15:27:40 +0200

    Implemented search dialog

--- app/src/main/java/nl/appsint/musetest/MainActivity.kt
+++ app/src/main/java/nl/appsint/musetest/MainActivity.kt
@@ -1,11 +1,88 @@
 package nl.appsint.musetest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.list.customListAdapter
+import com.choosemuse.libmuse.Muse
+import com.choosemuse.libmuse.MuseListener
+import com.choosemuse.libmuse.MuseManagerAndroid
+import nl.appsint.musetest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : PermissionActivity() {
+    private lateinit var museManager: MuseManagerAndroid
+    private lateinit var views: ActivityMainBinding
+    private lateinit var dialog: MaterialDialog
+
+    private val museListAdapter = MuseListAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        views = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(views.root)
+
+        dialog = MaterialDialog(this).show {
+            customListAdapter(museListAdapter)
+            title(R.string.select_muse)
+            cancelable(false)
+        }
+    }
+
+    override fun onPermissionsInitialized() {
+        initializeMuse()
+    }
+
+    private fun initializeMuse() {
+        museManager = MuseManagerAndroid.getInstance()
+        museManager.setContext(this)
+        museManager.setMuseListener(object : MuseListener() {
+            override fun museListChanged() {
+                museListAdapter.muses = museManager.muses
+            }
+        })
+
+        museManager.startListening()
+        dialog.show()
+    }
+
+    private fun onMuseSelected(muse: Muse) {
+        dialog.dismiss()
+    }
+
+    private inner class MuseListItem(parent: ViewGroup): RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_muse, parent, false)) {
+        fun bind(muse: Muse) {
+            itemView.findViewById<TextView>(R.id.name).text = muse.name
+            itemView.findViewById<TextView>(R.id.mac).text = muse.macAddress
+            itemView.setOnClickListener {
+                onMuseSelected(muse)
+            }
+        }
+    }
+
+    private inner class MuseListAdapter: RecyclerView.Adapter<MuseListItem>() {
+        var muses: ArrayList<Muse>? = null
+            @SuppressLint("NotifyDataSetChanged")
+            set(value) {
+                field = value
+                notifyDataSetChanged()
+            }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MuseListItem {
+            return MuseListItem(parent)
+        }
+
+        override fun onBindViewHolder(holder: MuseListItem, position: Int) {
+            muses?.get(position)?.let { muse ->
+                holder.bind(muse)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return muses?.size ?: 0
+        }
     }
 }
---
+++ app/src/main/java/nl/appsint/musetest/PermissionActivity.kt
@@ -0,0 +1,133 @@
+package nl.appsint.musetest
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+
+/**
+ * Created by APPSINT/thijs on oktober 29, 2021.
+ * Written for client "MuseTest"
+ * Package: nl.appsint.musetest
+ */
+abstract class PermissionActivity: AppCompatActivity() {
+    protected abstract fun onPermissionsInitialized()
+
+    private val requestEnableBluetooth = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        onBluetoothState(result.resultCode == RESULT_OK)
+    }
+
+    private val requestEnableLocation = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+        onPermissionState(result.resultCode == RESULT_OK)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkPermissionState()
+    }
+
+
+
+    // The following functions are called in order.
+
+    private fun checkPermissionState() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSIONS)
+        } else {
+            onPermissionState(granted = true)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_PERMISSIONS) {
+            onPermissionState(permissions.isNotEmpty() &&
+                    permissions.size == grantResults.size &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+            )
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+
+    }
+
+    private fun onPermissionState(granted: Boolean) {
+        if (granted) {
+            checkBluetoothState()
+        } else {
+            finish()
+        }
+    }
+
+    private fun checkBluetoothState() {
+        val enabled = (getSystemService(BLUETOOTH_SERVICE) as BluetoothManager?)?.adapter?.isEnabled
+        if (enabled == false) {
+            requestEnableBluetooth.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
+        } else {
+            onBluetoothState(enabled == true)
+        }
+    }
+
+    private fun onBluetoothState(available: Boolean) {
+        if (available) {
+            checkLocationServicesState()
+        } else {
+            finish()
+        }
+    }
+
+    private fun checkLocationServicesState() {
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_LOW_POWER
+        locationRequest.interval = 100000
+        locationRequest.fastestInterval = 10000L
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        builder.setAlwaysShow(true)
+
+        val client = LocationServices.getSettingsClient(this)
+
+        val result: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+        result.addOnCompleteListener { task ->
+            try {
+                task.getResult(ApiException::class.java)
+                onLocationServicesState(true)
+            } catch (ex: ApiException) {
+                when (ex.statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                        requestEnableLocation.launch(IntentSenderRequest.Builder((ex as ResolvableApiException).resolution).build())
+                    }
+
+                    else -> {
+                        onLocationServicesState(false)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun onLocationServicesState(available: Boolean) {
+        if (available) {
+            Handler(Looper.getMainLooper()).post {
+                onPermissionsInitialized()
+            }
+        } else {
+            finish()
+        }
+    }
+
+    private companion object {
+        private const val REQUEST_PERMISSIONS = 0x111
+    }
+}
